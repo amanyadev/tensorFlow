74b1fb02c0a34db31489e4f7dd8c7e77d8d26a7e
Aman Kumar Yadav <yadav.aman099@gmail.com>
2018-08-19 14:27:08 +0530

    Ready for training #ball spawns randomly however, that will need longer train time. still it works

---
+++ Pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	[SerializeField]
+	Rigidbody2D rb ;
+	[SerializeField]
+	float minSpeed = 5f,maxSpeed = 11f;
+	float velocity =0;
+
+	//for audio
+
+	public AudioSource source;
+	public AudioClip bip;
+	public AudioClip paddlepop;
+	public AudioClip crash;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		source = GetComponent<AudioSource>();
+		velocity = Random .Range(minSpeed, maxSpeed);
+		float service = Random .Range(0f, 2f);
+		Vector2 direction;
+		float angle= Mathf.Deg2Rad*0;
+		if(service>=1f){
+			angle = Random.Range (50, -50);
+		}else if (service<1f){
+			angle = Random.Range (130, 230);
+		}
+//		Debug.Log ("Service Angle" + angle+"Speed "+ velocity);
+		direction = new Vector2 (Mathf.Cos (angle*Mathf.Deg2Rad), Mathf.Sin (angle*Mathf.Deg2Rad));
+		rb.AddForce (direction * velocity, ForceMode2D.Impulse);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		/*if (rb.transform.position.x <=-11f||rb.transform.position.x >=11f) {
+			source.PlayOneShot (crash);
+		}*/
+
+
+	}
+	void OnCollisionEnter2D(Collision2D col){
+		string col_id = col.gameObject.tag;
+
+
+		if(col_id=="paddle"){
+			source.PlayOneShot(paddlepop);
+		}
+		if(col_id=="topwall"||col_id=="bottomwall"){
+			source.PlayOneShot(bip);
+		}
+			velocity *= 1.05f;
+//			Debug.Log (transform.position.x);
+	
+		
+	}
+}
---
+++ Pong/Assets/ml-agents-master/unity-environment/Assets/ML-Agents/Examples/Pong/paddleAgent.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+//used to control the paddle.
+public class paddleAgent : Agent 
+{
+	//THE AGENT CONTROLS THE PADDLE SO IT SHOULD KNOW ABOUT THE BALL.
+	//therefore we add a gameobject..
+	public GameObject ball;
+	public float vel;
+	public float speed = 5f;
+	public Rigidbody2D rb;
+
+	public override void InitializeAgent()
+	{
+		rb = gameObject.GetComponent<Rigidbody2D>()
+		;
+	}
+
+
+
+
+	public override void AgentAction(float[] act,string textAction){
+		//descrete are exact values 1 0 1.. continuous is any val btw 0 1 -1 etc.
+		//if(brain.brainParameters.vectorActionSpaceType == SpaceType.continuous){
+			/*float action = act [0];*/
+		/*Debug.Log (act [0]);*/
+			vel = rb.velocity.y;
+		/*Debug.Log (vel);*/
+			float location = gameObject.GetComponent<Transform> ().position.y;
+			if (location < 4.8f && location > -4.8f) {
+				vel = act [0] * speed;
+			Debug.Log ("act is "+act [0]);
+			rb.velocity = new Vector2 (rb.velocity.x, vel);
+			AddReward (.001f);
+
+			}
+			else{
+				if(location>4.8f){
+					location = 4.8f;
+				}
+				if(location<-4.8f){
+					location = -4.8f;
+				}
+				Vector2 loc = new Vector2 (gameObject.transform.position.x, location);
+				location = loc.y;
+				
+			}
+		//	if(ball.transform.position.y>transform.position.y){
+
+			//}
+		if(ball.transform.position.x>11f){
+			AddReward (.1f);
+			/*Debug.Log("rewarded+1");*/
+		}
+		if(ball.transform.position.x<-11f){
+			AddReward(-1f);
+			/*Debug.Log("rewarded -1");*/
+		//	Debug.Log (ball.transform.position.x);
+			gameObject.transform.position = new Vector2 (-10f, 0f);
+		//	Debug.Log ("Done  agent");
+			Done ();
+
+		}
+		else{
+		}
+	}
+   
+
+	//not controlled by start and update rather controlled by the ML agents..
+	public override void  CollectObservations(){
+		//getting state. of paddle as well as ball.
+		//List <float> state = new List<float> ();
+		AddVectorObs (gameObject.transform.position.x);
+		AddVectorObs (gameObject.transform.position.y);
+		AddVectorObs (ball.transform.position.x - gameObject.transform.position.x);
+		AddVectorObs(ball.transform.position.y - gameObject.transform.position.y);
+		AddVectorObs (ball.transform.GetComponent<Rigidbody2D> ().velocity.x);
+		AddVectorObs (ball.transform.GetComponent<Rigidbody2D> ().velocity.y);
+	}
+
+	public override void AgentReset ()
+	{
+		
+		/*Debug.Log ("Done called agent");*/
+			//reset our agent
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.tag == "paddle"){
+			AddReward (0.1f);
+			Debug.Log("rewardedc+1");
+		}
+	}
+
+}
